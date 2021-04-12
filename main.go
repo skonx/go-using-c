6c9c6af9689f491638c2876afd04d249abf62013
Julien Sié <julien.sie@gmail.com>
2021-04-12 19:59:41 +0200

    display a fish pyramid

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 /*
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void hello()
+void hello(char *s, char *fish)
 {
-	printf("Hello World !\n");
+    int i, j;
+    int N = 10;
+    char *fishes = malloc((strlen(fish) * N + 1) * sizeof(char));
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < i + 1; j++)
+        {
+            strcat(fishes, fish);
+        }
+        printf("Hello %s %s\n", s, fishes);
+        fishes[0] = '\0'; // reset the string
+    }
+    free(fishes);
 }
 */
 import "C"
 
 func main() {
-	C.hello()
+	C.hello(C.CString("C-World"), C.CString("🐟"))
 }
